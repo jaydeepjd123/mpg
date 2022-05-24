5fac86fbcc769de30e99a850960c5b4403936615
jaydeepjd123 <jaydeeppatel100@gmail.com>
2022-05-24 18:50:46 -0400

    Task is done

---
+++ mpg.js
@@ -0,0 +1,44 @@
+"use strict";
+
+
+const $ = selector => document.querySelector(selector);
+
+const getErrorMsg = lbl => `${lbl} must be a valid number greater than zero.`;
+
+const focusAndSelect = selector => {
+    const elem = $(selector);
+    elem.focus();
+    elem.select();
+};
+
+const processEntries = () => {
+    const miles = parseFloat($("#miles").value);
+    const gallons = parseFloat($("#gallons").value);
+
+    if (isNaN(miles) && miles <= 0) {
+        alert(getErrorMsg("Miles driven"));
+        focusAndSelect("#miles");
+    } else if (isNaN(gallons) && gallons <= 0) {
+        alert(getErrorMsg("Gallons of gas used"));
+        focusAndSelect("#gallons");
+    } else {
+        $("#mpg").value = (miles / gallons).toFixed(2);
+    }
+};
+
+const clearEntries = () => {
+    $("#miles").value = "";
+    $("#gallons").value = "";
+    $("#mpg").value = "";
+    alert("You have doubleclicked. \n Data will gets clear automatically");
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+    $("#gallons").addEventListener("focusout", processEntries);
+    $("#miles").focus();
+});
+document.addEventListener("DOMContentLoaded", () => {
+       $("#mpg").addEventListener("dblclick", clearEntries);
+ 
+    
+});
